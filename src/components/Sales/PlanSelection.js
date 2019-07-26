32e0018790ff1710168639e071a2a2a49786d0c8
Victor G <victorgordian103@gmail.com>
2019-07-25 22:51:25 -0700

    created if else if logic

---
+++ src/components/Sales/PlanSelection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Card } from '../../assets/css/layer'
+const PlanSelection = (props) => {
+  return (
+    <div>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  )
+}
+
+export default PlanSelection;
--- src/components/Sales/SalesPage.js
+++ src/components/Sales/SalesPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import InfoContainer from './InfoContainer'
+import PlanSelction from './PlanSelection'
 
 import '../../assets/css/sales.css'
 
@@ -9,24 +10,41 @@ class Sales extends Component {
         this.state = {
             user_name: "",
             piece: "",
-            first: false,
         }
     }
 
     clickedA = (e) => {
-        this.setState({ piece: "Bong", first: true })
+        this.setState({ piece: "Bong", })
     }
     clickedB = (e) => {
-        this.setState({ piece: "Pipe", first: true })
+        this.setState({ piece: "Pipe", })
+    }
+
+    secondQuestion() {
+
+        if (this.state.piece === 'Bong') {
+            return (
+
+                <PlanSelction />
+
+            )
+        } else if (this.state.piece === 'Pipe') {
+            return (
+                <InfoContainer />
+            )
+        } else {
+            return (
+                null
+            )
+        }
     }
 
     render() {
         return (
             <div className='form-wrapper'>
                 <InfoContainer a={this.clickedA} b={this.clickedB} />
-                {this.state.first ? (
-                    <div className=''></div>
-                ) : null}
+                {this.secondQuestion()}
+
             </div>
         )
     }
