def4b502be57bac632b460ec8f97e2278815fe5a
Victor G <victorgordian103@gmail.com>
2020-04-16 00:41:21 -0700

    better styled

--- src/components/DashBoard/ViewItem.js
+++ src/components/DashBoard/ViewItem.js
@@ -40,25 +40,37 @@ class ViewItem extends Component {
         </div>
 
 
+        <div className='detail-wrapper'>
+          <div className='item-description'>
 
-        <div className='item-description'>
-          <h1>Brand Name</h1>
-          <h1>Beaker</h1>
-          <h1>Price</h1>
-          <button>Add to Cart</button>
+            <div className='price-name'>
+              <h2>$ 89.99</h2>
+              <h1>USA Bent Neck Bong</h1>
+            </div>
+            <div className='location'>
+              <h2>South Gate</h2>
+            </div>
 
-          <div>
-            This is the classic beaker tube by Maverick Glass. This 18'' tall bong features a 18mm to 14mm removable diffused downstem and comes with a 14mm male bowl. The ice pinch allows you to stack ice for an extra level of cooling. This highly functional bong will be sure to please and may quickly become your daily driver. The glass is 5mm thick. Made by Maverick Glass in California using high quality borosilicate glass.
+
+            <div className='summary'>
+              This is the classic beaker tube by Maverick Glass. This 18'' tall bong features a 18mm to 14mm removable diffused downstem and comes with a 14mm male bowl. The ice pinch allows you to stack ice for an extra level of cooling. This highly functional bong will be sure to please and may quickly become your daily driver. The glass is 5mm thick. Made by Maverick Glass in California using high quality borosilicate glass.
           </div>
-          <div>
-            <p>Beaker Base Bong</p>
-            <p>18mm to 14mm Diffused Downstem</p>
-            <p>14mm Male bowl</p>
-            <p>Ice Pinch</p>
-            <p>18'' Inches Tall</p>
-            <p>5mm Thick</p>
+            <div className='details'>
+              <p>•Beaker Base Bong</p>
+              <p>•18mm to 14mm Diffused Downstem</p>
+              <p>•14mm Male bowl</p>
+              <p>•Ice Pinch</p>
+              <p>•18'' Inches Tall</p>
+              <p>•5mm Thick</p>
+            </div>
+          </div>
+          <div className='right-wrapper'>
+            <h1>Tasty Hits Shop</h1>
+            <button>Login in</button>
+            <button>Sign up</button>
           </div>
         </div>
+
       </div>
     )
   }
